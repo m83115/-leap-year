5ef7f688a519c78f80e1296684ca4ffc8f90f51f
m83115 <m83115@yahoo.com.tw>
2021-07-08 19:55:04 +0800

    day01

---
+++ 01/a.js
@@ -0,0 +1,75 @@
+// console.log("hi 中文也行")
+// console.log("   hi 中文也行")
+// console.log("hello world")
+
+// console.log("hi 中文也行");
+// console.log('hi 中文也行');
+// console.log(`hi 中文也行`);
+
+// 各種引號都可以!分號要加不加都可以!
+
+
+//變數 variabla 定義(宣告)
+
+// var a = "hi"
+
+// console.log(a)
+// console.log("a")
+
+// var a
+// console.log(a)
+
+// undefined 未定義 有容器沒值
+// not define 沒定義 跟本沒有容器
+// unll 空值
+
+var a = 1
+var b = "2"
+console.log(a + b)
+
+// 會印出12，JS不同型態會自動轉型!
+
+
+// JS的資料型態:
+
+// -3(數字)(Number)
+// -"hello"(字串)(String)
+// -undefined(Undefined)
+// -unll(Null)
+// -true/false(真假值/布林值)(Boolean)
+
+// -[     ](陣列)(Array)
+// [1, 2, "aa", "xyz", true, null]
+
+// -{     }(物件)(Object)
+// { 
+//     name: "kk", 
+//     age: 18, 
+//     isAdult: false 
+//   }
+
+
+變數提昇 variable hoising
+
+JS會分2階段處理!
+建立期+執行期
+
+console.log(a) //undefiend
+var a = 1
+
+
+ECMA -> ECMAScript
+
+2015 -> 
+ES6 == ES2015(教的以這為主!)
+
+ES7 == ES2016
+ES8 == ES2017
+ES11 == ES2020
+
+
+JS 是一種 ECMAScript
+
+TC39 負責 JS
+TC39 屬於 ECMA 組織
+
