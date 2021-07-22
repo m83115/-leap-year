30093ce0e015c5dad0f842c8d999fc7905686cfc
m83115 <m83115@yahoo.com.tw>
2021-07-23 00:42:25 +0800

    05-w1

--- homework/05-w1.js
+++ homework/05-w1.js
@@ -1,6 +1,10 @@
 const lists = [2, 4, 1, 8, 7, 3, 5]
 
-// 實作...
+function greaterThan(n){
+  return function(x){
+    return x > n   
+  }
+}
 
 const result = lists.filter(greaterThan(4))
 console.log(result)  // 印出 [ 8, 7, 5 ]
--- homework/script.js
+++ homework/script.js
@@ -1,13 +1,10 @@
-
 document.addEventListener("DOMContentLoaded", () => {
   const timer = document.querySelector("#timer")
   const btn = document.querySelector("#start-btn")
-
-  
   btn.addEventListener("click", () => {
     let num = 0
-    let i = setInterval(() => {
-      timer.innerText = num++
+    setInterval(() => {
+      timer.innerHTML = num++
     }, 1000)
   })
 })
